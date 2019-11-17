6aa548bddf93b09967c17cea946839eccfa35c3b
Kshamina <kshamina.ghelani@carleton.ca>
2019-11-17 15:42:46 -0500

    Updates

---
+++ precache-manifest.2b085f7b74f1872db556379a37793803.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "468c218f9182c3ea8c69ac0b06f10638",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67ee588e8b6a16fd19c9",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "fe87dfa3b21ed67d9c5d",
+    "url": "/static/js/2.cc894e8a.chunk.js"
+  },
+  {
+    "revision": "67ee588e8b6a16fd19c9",
+    "url": "/static/js/main.f3f4501b.chunk.js"
+  },
+  {
+    "revision": "7e2ee6aa58c7c8f80ab6",
+    "url": "/static/js/runtime-main.55ac1415.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
